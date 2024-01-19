1e93bcd477c7465c82cbf4a995be6e3551c56613
RupertJelnikarMrak <jelnikar.mrak.rupert+github@gmail.com>
2024-01-19 23:45:28 +0100

    Added a check for correct tool

--- src/main/java/com/bobigrenade/ahtweaks/util/HarvestBlockHandler.java
+++ src/main/java/com/bobigrenade/ahtweaks/util/HarvestBlockHandler.java
@@ -5,26 +5,38 @@ import com.bobigrenade.ahtweaks.AHTweaksConfig;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.ToolType;
 
 import java.util.List;
 
 public class HarvestBlockHandler {
-    
+    public static final ToolType SWORD = ToolType.get("sword");
+
+
     public static boolean canBreak(BlockState state, PlayerEntity player)
     {
         boolean can_break = true;
+        ItemStack held_item = player.getHeldItemMainhand();
 
         Block block = state.getBlock();
         List<String> tag_whitelist = AHTweaksConfig.blockTagWhitelist.get();
 
         for (String tag: tag_whitelist)
             if (block.getTags().contains(new ResourceLocation(tag))){
-                can_break = false;
+                can_break = canHarvestWithTool(state, player, held_item);
                 break;
             }
 
         return can_break;
+    }
+
+    private static boolean canHarvestWithTool(BlockState state, PlayerEntity player, ItemStack item_stack)
+    {
+        return item_stack.getToolTypes().contains(state.getHarvestTool());
+       
+        // *TODO: Add special checks for modded tools and levels
     }
 
 }
