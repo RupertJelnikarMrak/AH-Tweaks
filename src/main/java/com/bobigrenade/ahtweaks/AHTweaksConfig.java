adf0af286e39fe9b5658a6c8dc1fc9a0237a53a4
RupertJelnikarMrak <jelnikar.mrak.rupert+github@gmail.com>
2024-01-20 01:09:41 +0100

    Added player damage tick config

--- src/main/java/com/bobigrenade/ahtweaks/AHTweaksConfig.java
+++ src/main/java/com/bobigrenade/ahtweaks/AHTweaksConfig.java
@@ -12,19 +12,33 @@ public class AHTweaksConfig {
     public static final ForgeConfigSpec spec;
 
     public static final ForgeConfigSpec.ConfigValue<List<String>> blockTagWhitelist;
+    public static final ForgeConfigSpec.BooleanValue unbreakableBlocks;
     public static final ForgeConfigSpec.BooleanValue damagePlayer;
+    public static final ForgeConfigSpec.ConfigValue<Integer> damageTick;
 
     static {
 
         builder.comment("AH Tweaks Configuration").push("common");
 
         blockTagWhitelist = builder
-            .comment("List of block tags that will require their respected tool to be used")
+            .comment("List of block tags that will be affected by this mod")
             .define("BlockTag Whitelist", Arrays.asList("minecraft:logs", "minecraft:planks", "minecraft:wooden_stairs", "minecraft:wooden_slabs", "minecraft:wooden_fences"));
 
+        builder.comment("\n");
+
+        unbreakableBlocks = builder
+            .comment("If true, whitelisted blocks will be unbreakable without using the correct tool \nDefault: true")
+            .define("Unbreakable Blocks", true);
+
+        builder.comment("Settings related to player damage").push("PlayerDamage");
+
         damagePlayer = builder
-            .comment("If true, the player will take damage when they try to break a whitelisted block without a tool")
+            .comment("If true, whitelisted blocks will damage the player when punched with bare hands \nDefault: true")
             .define("Damage Player", true);
+
+        damageTick = builder
+            .comment("The every of this number of ticks the player will take damage when breaking blocks (higher value less damage). Ignored if damagePlayer is false. \nRange: >1, default: 30")
+            .define("Damage Tickrate", 30);
 
         builder.pop();
         spec = builder.build();
--- src/main/java/com/bobigrenade/ahtweaks/ForgeEventHandler.java
+++ src/main/java/com/bobigrenade/ahtweaks/ForgeEventHandler.java
@@ -10,6 +10,7 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 
 public class ForgeEventHandler {
+    public static Integer damageTickCount = 0;
 
     public static void init()
     {
@@ -21,11 +22,21 @@ public class ForgeEventHandler {
         PlayerEntity player = event.getPlayer();
         if (!HarvestBlockHandler.canBreak(event.getState(), player))
         {
-            float newSpeed = 1e-10f;
-            event.setNewSpeed(newSpeed);
-            if (player.getHeldItemMainhand() == ItemStack.EMPTY)
+            if (AHTweaksConfig.unbreakableBlocks.get())
             {
-                player.attackEntityFrom(DamageSource.GENERIC, 1);
+                float newSpeed = 1e-10f;
+                event.setNewSpeed(newSpeed);
+            }
+
+            if (player.getHeldItemMainhand() == ItemStack.EMPTY && AHTweaksConfig.damagePlayer.get())
+            {
+                if (damageTickCount < AHTweaksConfig.damageTick.get())
+                    damageTickCount++;
+                else
+                {
+                    damageTickCount = 0;
+                    player.attackEntityFrom(DamageSource.GENERIC, 1);
+                }
             }
         }
     }
